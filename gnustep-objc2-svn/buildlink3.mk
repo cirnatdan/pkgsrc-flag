1b9b86bd19fc05ae8866a6d956b3dc8cc1309400
Dan Cirnat <dan@compunet.md>
2015-01-17 15:03:32 +0200

    fixed build, now with cmake

--- gnustep-objc2-svn/buildlink3.mk
+++ gnustep-objc2-svn/buildlink3.mk
@@ -2,8 +2,8 @@
 
 BUILDLINK_TREE+=	gnustep-objc2
 
-.if !defined(GNUSTEP_OBJC_BUILDLINK3_MK)
-GNUSTEP_OBJC_BUILDLINK3_MK:=
+.if !defined(GNUSTEP_OBJC2_BUILDLINK3_MK)
+GNUSTEP_OBJC2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnustep-objc2+=	gnustep-objc2>=201103
 BUILDLINK_ABI_DEPENDS.gnustep-objc2?=	gnustep-objc2>=201103
@@ -11,7 +11,7 @@ BUILDLINK_PKGSRCDIR.gnustep-objc2?=	../../devel/gnustep-objc
 
 BUILDLINK_LDFLAGS.gnustep-objc2+=	-lobjc -lpthread
 
-.include "../../flag/gnustep-make-svn/buildlink3.mk"
+#.include "../../flag/gnustep-make-svn/buildlink3.mk"
 .endif # GNUSTEP_OBJC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnustep-objc2
