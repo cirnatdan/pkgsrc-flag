25d7de7b692d001fd4ed8d5ef980e6f3b03431bc
Dan Cîrnaț <dan@alt.md>
2020-05-08 16:27:04 +0200

    No longer using cmake

--- butterlibc-git/buildlink3.mk
+++ butterlibc-git/buildlink3.mk
@@ -8,6 +8,7 @@ BUTTERLIBC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.butterlibc+=	butterlibc>=0.0.0nb20200330
 BUILDLINK_ABI_DEPENDS.butterlibc+=	butterlibc>=0.0.0nb20200330
 BUILDLINK_PKGSRCDIR.butterlibc?=	../../flag/butterlibc-git
+BUILDLINK_FILES.butterlibc+=		lib/crt0.o
 .endif	# BUTTERLIBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-butterlibc
